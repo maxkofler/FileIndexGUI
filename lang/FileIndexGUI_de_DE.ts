e1294595fbf7b4a0aadc6dc78d55cab47334c2b3
Max Kofler <kofler.max.dev@gmail.com>
2022-11-12 14:40:17 +0100

    Translated some new strings

--- lang/FileIndexGUI_de_DE.ts
+++ lang/FileIndexGUI_de_DE.ts
@@ -18,30 +18,30 @@
         <location filename="../gui/ui/indexdialog.ui" line="14"/>
         <location filename="../build/ui/ui_indexdialog.h" line="92"/>
         <source>Index</source>
-        <translation type="unfinished">Neues Dateisystem indizieren</translation>
+        <translation>Neuer Index</translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="48"/>
         <location filename="../build/ui/ui_indexdialog.h" line="93"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="64"/>
         <location filename="../build/ui/ui_indexdialog.h" line="94"/>
         <source>Path</source>
-        <translation type="unfinished"></translation>
+        <translation>Pfad</translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="77"/>
         <location filename="../build/ui/ui_indexdialog.h" line="95"/>
         <source>Crate (optional)</source>
-        <translation type="unfinished"></translation>
+        <translation>Kartei (optional)</translation>
     </message>
     <message>
         <location filename="../gui/src/indexdialog/indexdialog.cpp" line="27"/>
         <source>Select the directory to index</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzeichnis auswählen</translation>
     </message>
 </context>
 <context>
@@ -50,116 +50,122 @@
         <location filename="../gui/ui/mainwindow.ui" line="14"/>
         <location filename="../build/ui/ui_mainwindow.h" line="95"/>
         <source>FileIndexGUI</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="37"/>
         <location filename="../build/ui/ui_mainwindow.h" line="112"/>
         <source>File</source>
-        <translation type="unfinished">Datei</translation>
+        <translation>Datei</translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="51"/>
         <location filename="../build/ui/ui_mainwindow.h" line="96"/>
         <source>Index</source>
-        <translation type="unfinished">Neues Dateisystem indizieren</translation>
+        <translation>Neuer index</translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="54"/>
         <location filename="../build/ui/ui_mainwindow.h" line="98"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="62"/>
         <location filename="../build/ui/ui_mainwindow.h" line="100"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Speichern</translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="65"/>
         <location filename="../build/ui/ui_mainwindow.h" line="102"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="70"/>
         <location filename="../build/ui/ui_mainwindow.h" line="104"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Öffnen</translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="73"/>
         <location filename="../build/ui/ui_mainwindow.h" line="106"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="78"/>
         <location filename="../build/ui/ui_mainwindow.h" line="108"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="81"/>
         <location filename="../build/ui/ui_mainwindow.h" line="110"/>
         <source>Ctrl+,</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="98"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="102"/>
         <source>Save FileIndex Database</source>
-        <translation type="unfinished"></translation>
+        <translation>Datenbank speichern</translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="98"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="102"/>
         <source>FileIndex Database (*.fidb)</source>
-        <translation type="unfinished"></translation>
+        <translation>FileIndex Datenbank</translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="117"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="121"/>
         <source>Select the file to import</source>
-        <translation type="unfinished"></translation>
+        <translation>Datei wählen</translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../gui/src/main.cpp" line="30"/>
+        <location filename="../gui/src/main.cpp" line="31"/>
         <source>Starting up...</source>
-        <translation type="unfinished">Starte...</translation>
+        <translation>Starte...</translation>
     </message>
 </context>
 <context>
     <name>SettingsDialog</name>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="14"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="71"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="76"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="32"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="72"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="77"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="48"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="73"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="78"/>
         <source>Startup Database</source>
-        <translation type="unfinished"></translation>
+        <translation>Startdatenbank</translation>
     </message>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="61"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="74"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="79"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/settingsdialog/settingsdialog.cpp" line="30"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="77"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="80"/>
+        <source>Match search casing</source>
+        <translation>G/Kleinschreibung</translation>
+    </message>
+    <message>
+        <location filename="../gui/src/settingsdialog/settingsdialog.cpp" line="31"/>
         <source>Select the database to load on startup</source>
-        <translation type="unfinished"></translation>
+        <translation>Startdatenbank auswählen</translation>
     </message>
 </context>
 </TS>
--- lang/FileIndexGUI_en_US.ts
+++ lang/FileIndexGUI_en_US.ts
@@ -18,30 +18,30 @@
         <location filename="../gui/ui/indexdialog.ui" line="14"/>
         <location filename="../build/ui/ui_indexdialog.h" line="92"/>
         <source>Index</source>
-        <translation type="unfinished">Index new Filesystem</translation>
+        <translation>Index new Filesystem</translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="48"/>
         <location filename="../build/ui/ui_indexdialog.h" line="93"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="64"/>
         <location filename="../build/ui/ui_indexdialog.h" line="94"/>
         <source>Path</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="77"/>
         <location filename="../build/ui/ui_indexdialog.h" line="95"/>
         <source>Crate (optional)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/src/indexdialog/indexdialog.cpp" line="27"/>
         <source>Select the directory to index</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -50,116 +50,122 @@
         <location filename="../gui/ui/mainwindow.ui" line="14"/>
         <location filename="../build/ui/ui_mainwindow.h" line="95"/>
         <source>FileIndexGUI</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="37"/>
         <location filename="../build/ui/ui_mainwindow.h" line="112"/>
         <source>File</source>
-        <translation type="unfinished">File</translation>
+        <translation>File</translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="51"/>
         <location filename="../build/ui/ui_mainwindow.h" line="96"/>
         <source>Index</source>
-        <translation type="unfinished">Index new Filesystem</translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="54"/>
         <location filename="../build/ui/ui_mainwindow.h" line="98"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="62"/>
         <location filename="../build/ui/ui_mainwindow.h" line="100"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="65"/>
         <location filename="../build/ui/ui_mainwindow.h" line="102"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="70"/>
         <location filename="../build/ui/ui_mainwindow.h" line="104"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="73"/>
         <location filename="../build/ui/ui_mainwindow.h" line="106"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="78"/>
         <location filename="../build/ui/ui_mainwindow.h" line="108"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="81"/>
         <location filename="../build/ui/ui_mainwindow.h" line="110"/>
         <source>Ctrl+,</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="98"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="102"/>
         <source>Save FileIndex Database</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="98"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="102"/>
         <source>FileIndex Database (*.fidb)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="117"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="121"/>
         <source>Select the file to import</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../gui/src/main.cpp" line="30"/>
+        <location filename="../gui/src/main.cpp" line="31"/>
         <source>Starting up...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>SettingsDialog</name>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="14"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="71"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="76"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="32"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="72"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="77"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="48"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="73"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="78"/>
         <source>Startup Database</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/settingsdialog.ui" line="61"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="74"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="79"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/settingsdialog/settingsdialog.cpp" line="30"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="77"/>
+        <location filename="../build/ui/ui_settingsdialog.h" line="80"/>
+        <source>Match search casing</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/src/settingsdialog/settingsdialog.cpp" line="31"/>
         <source>Select the database to load on startup</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
