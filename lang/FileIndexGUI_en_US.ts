6f7d3f71379ebcc073c8d3ee1ba7c96bd82adb2a
Max Kofler <kofler.max.dev@gmail.com>
2023-01-04 16:40:20 +0100

    Update Translations

--- lang/FileIndexGUI_en_US.ts
+++ lang/FileIndexGUI_en_US.ts
@@ -16,25 +16,25 @@
     <name>IndexDialog</name>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="14"/>
-        <location filename="../build/ui/ui_indexdialog.h" line="92"/>
+        <location filename="../buildRel/ui/ui_indexdialog.h" line="92"/>
         <source>Index</source>
         <translation>Index new Filesystem</translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="48"/>
-        <location filename="../build/ui/ui_indexdialog.h" line="93"/>
+        <location filename="../buildRel/ui/ui_indexdialog.h" line="93"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="64"/>
-        <location filename="../build/ui/ui_indexdialog.h" line="94"/>
+        <location filename="../buildRel/ui/ui_indexdialog.h" line="94"/>
         <source>Path</source>
         <translation></translation>
     </message>
     <message>
         <location filename="../gui/ui/indexdialog.ui" line="77"/>
-        <location filename="../build/ui/ui_indexdialog.h" line="95"/>
+        <location filename="../buildRel/ui/ui_indexdialog.h" line="95"/>
         <source>Crate (optional)</source>
         <translation></translation>
     </message>
@@ -48,76 +48,100 @@
     <name>MainWindow</name>
     <message>
         <location filename="../gui/ui/mainwindow.ui" line="14"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="95"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="116"/>
         <source>FileIndexGUI</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="37"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="112"/>
+        <location filename="../gui/ui/mainwindow.ui" line="32"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="135"/>
+        <source>Ctrl+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="29"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="133"/>
+        <source>Collapse All (C)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="39"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="137"/>
+        <source>Expand All (E)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="42"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="139"/>
+        <source>Ctrl+E</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/mainwindow.ui" line="61"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="141"/>
         <source>File</source>
         <translation>File</translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="51"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="96"/>
+        <location filename="../gui/ui/mainwindow.ui" line="75"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="117"/>
         <source>Index</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="54"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="98"/>
+        <location filename="../gui/ui/mainwindow.ui" line="78"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="119"/>
         <source>Ctrl+I</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="62"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="100"/>
+        <location filename="../gui/ui/mainwindow.ui" line="86"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="121"/>
         <source>Save</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="65"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="102"/>
+        <location filename="../gui/ui/mainwindow.ui" line="89"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="123"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="70"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="104"/>
+        <location filename="../gui/ui/mainwindow.ui" line="94"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="125"/>
         <source>Open</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="73"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="106"/>
+        <location filename="../gui/ui/mainwindow.ui" line="97"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="127"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="78"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="108"/>
+        <location filename="../gui/ui/mainwindow.ui" line="102"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="129"/>
         <source>Settings</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/mainwindow.ui" line="81"/>
-        <location filename="../build/ui/ui_mainwindow.h" line="110"/>
+        <location filename="../gui/ui/mainwindow.ui" line="105"/>
+        <location filename="../buildRel/ui/ui_mainwindow.h" line="131"/>
         <source>Ctrl+,</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="102"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="98"/>
         <source>Save FileIndex Database</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="102"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="98"/>
         <source>FileIndex Database (*.fidb)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="121"/>
+        <location filename="../gui/src/mainwindow/mainwindow.cpp" line="111"/>
         <source>Select the file to import</source>
         <translation></translation>
     </message>
@@ -125,7 +149,7 @@
 <context>
     <name>QApplication</name>
     <message>
-        <location filename="../gui/src/main.cpp" line="31"/>
+        <location filename="../gui/src/main.cpp" line="38"/>
         <source>Starting up...</source>
         <translation></translation>
     </message>
@@ -133,37 +157,49 @@
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../gui/ui/settingsdialog.ui" line="14"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="76"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="20"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="118"/>
         <source>Dialog</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/settingsdialog.ui" line="32"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="77"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="60"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="120"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/settingsdialog.ui" line="48"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="78"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="45"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="119"/>
         <source>Startup Database</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/settingsdialog.ui" line="61"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="79"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="115"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="124"/>
         <source>OK</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/ui/settingsdialog.ui" line="77"/>
-        <location filename="../build/ui/ui_settingsdialog.h" line="80"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="106"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="123"/>
+        <source>Expand Tree when searching</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/ui/settingsdialog.ui" line="93"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="122"/>
         <source>Match search casing</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../gui/src/settingsdialog/settingsdialog.cpp" line="31"/>
+        <location filename="../gui/ui/settingsdialog.ui" line="80"/>
+        <location filename="../buildRel/ui/ui_settingsdialog.h" line="121"/>
+        <source>Search</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/src/settingsdialog/settingsdialog.cpp" line="32"/>
         <source>Select the database to load on startup</source>
         <translation></translation>
     </message>
